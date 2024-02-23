6482cabf51c230ee5815f79847e0770886b2628c
tahany777 <tahanymohammed777@hotmail.com>
2024-02-23 19:46:10 +0000

    last edit for deployment

--- db.js
+++ db.js
@@ -1,11 +1,13 @@
 const {Pool} = require("pg");
 
-const pool=new Pool({
-    user: "tahany",
-    password: "0502078454",
-    host: "localhost",
-    port:5432,
-    database: "perntodo"
-});
-
+// const pool=new Pool({
+//     user: "tahany",
+//     password: "0502078454",
+//     host: "localhost",
+//     port:5432,
+//     database: "perntodo"
+// });
+const pool = new Pool({
+    connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+})
 module.exports = pool;
