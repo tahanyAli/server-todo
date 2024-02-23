cc9da65cd1990b95f2295fbc72ac3bf970111c58
tahany777 <tahanymohammed777@hotmail.com>
2024-02-23 18:47:54 +0000

    first commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const pool = require("./db");
-
+const PORT = process.env.PORT || 5000;
 //middleware
 app.use(cors());
 app.use(express.json()); //req.body
@@ -69,7 +69,7 @@ app.delete("/todos/:id", async (req, res) => {
   }
 });
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
   console.log("server has started on port 5000");
 });
 
