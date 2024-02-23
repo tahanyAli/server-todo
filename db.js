2ff81dd101877fe3a7541a34e19d225fd217a173
tahany777 <tahanymohammed777@hotmail.com>
2024-02-23 20:21:04 +0000

    edit psql

--- db.js
+++ db.js
@@ -1,13 +1,19 @@
 const {Pool} = require("pg");
 
-// const pool=new Pool({
-//     user: "tahany",
-//     password: "0502078454",
-//     host: "localhost",
-//     port:5432,
-//     database: "perntodo"
-// });
-const pool = new Pool({
-    connectionString: process.env.POSTGRES_URL + "?sslmode=require",
-})
+const UserName=process.env.PG_USERNAME;
+const Host=process.env.PG_HOST;
+const Password=process.env.PG_PASSWORD;
+const Port=process.env.PG_PORT;
+const Database=process.env.PG_DATABASE;
+const pool=new Pool({
+    user: UserName,
+    password: Password,
+    host: Host,
+    port:Port,
+    database: Database
+});
+// postgres://perntodo_o4i5_user:trRabs70nWN0wjlDHiaVV0hU3XMpWePw@dpg-cncf7cect0pc73frdhl0-a/perntodo_o4i5
+// const pool = new Pool({
+//     connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+// })
 module.exports = pool;
